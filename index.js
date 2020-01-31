28fc44fdf267dbf6a720452940a8b504041d7b23
Worst-in-Me <34812286+Worst-in-Me@users.noreply.github.com>
2020-01-31 14:46:29 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const { reply, pause, audio, buttons } = require('alice-renderer');
-
-reply`
-    ${audio('sounds-game-win-1')} ${['Привет', 'т+аджикистан']}! ${pause(500)} Как дел+а?
-    ${buttons(['Все отлично', 'Супер'])}
-    `;
